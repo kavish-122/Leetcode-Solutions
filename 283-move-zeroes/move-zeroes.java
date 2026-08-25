96eea1b7a56eac0fe2780967704daa3e1796c130
Kavish Kamal <kavishkamal437@gmail.com>
2026-08-25 23:48:34 +0530

    Time: 2 ms (91.95%) | Memory: 48 MB (6.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,38 @@
 class Solution {
+    public void swap(int nums[], int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
     public void moveZeroes(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int k = 0;
+        int j=-1;
         for(int i=0; i<nums.length; i++){
-            if(nums[i] != 0){
-                map.put(k,nums[i]);
-                k++;
+            if(nums[i] == 0){
+                j=i;
+                break;
             }
         }
-        for(int i=0; i<map.size(); i++){
-            nums[i] = map.get(i);
+        if(j == -1) return;
+        for(int i=j+1; i<nums.length; i++){
+            if(nums[i] != 0){
+                swap(nums, i, j);
+                j++;
+            }
         }
-        for(int i=map.size(); i<nums.length; i++){
-            nums[i] = 0;
-        }
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // int k = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i] != 0){
+        //         map.put(k,nums[i]);
+        //         k++;
+        //     }
+        // }
+        // for(int i=0; i<map.size(); i++){
+        //     nums[i] = map.get(i);
+        // }
+        // for(int i=map.size(); i<nums.length; i++){
+        //     nums[i] = 0;
+        // }
         
     }
 }
