a96ca5d952b1ed346929102d7a18d9f2c45b778d
Kavish Kamal <kavishkamal437@gmail.com>
2026-08-25 19:52:31 +0530

    Time: 4 ms (15.69%) | Memory: 268.9 MB (5.62%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,20 +1,43 @@
 class Solution {
+    public void swap(int[] nums,int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    public void reverse(int[] nums, int low, int high){
+        while(low < high){
+            swap(nums,low,high);
+            low++;
+            high--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
+        //OPTIMAL SOLUTION
+        k = k%nums.length;
+        reverse(nums,0,nums.length-k-1);
+        reverse(nums,nums.length-k,nums.length-1);
+        reverse(nums, 0, nums.length-1);
 
-        int arr[] = new int[k];
-        for(int i = nums.length - k; i < nums.length; i++) {
-            arr[i - (nums.length - k)] = nums[i];
-        }
-        int m = nums.length - 1;
+        //USING TEMPERORY ARRAY
 
-        for(int i = nums.length - k - 1; i >= 0; i--) {
-            nums[m] = nums[i];
-            m--;
-        }
-        for(int i = 0; i < k; i++) {
-            nums[i] = arr[i];
-        }
+        // k = k % nums.length;
+
+        // int arr[] = new int[k];
+        // for(int i = nums.length - k; i < nums.length; i++) {
+        //     arr[i - (nums.length - k)] = nums[i];
+        // }
+        // int m = nums.length - 1;
+
+        // for(int i = nums.length - k - 1; i >= 0; i--) {
+        //     nums[m] = nums[i];
+        //     m--;
+        // }
+        // for(int i = 0; i < k; i++) {
+        //     nums[i] = arr[i];
+        // }
+
+        // USING HASHMAP
+
         // HashMap<Integer, Integer> map = new HashMap<>();
         // if(k>nums.length){
         //     k = k%nums.length;
