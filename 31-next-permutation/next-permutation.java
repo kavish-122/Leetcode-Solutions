a65d1268e0b1a30604b5d0bc4c829b58756363c4
Kavish Kamal <kavishkamal437@gmail.com>
2026-09-04 01:36:08 +0530

    Time: 2 ms (6.96%) | Memory: 44.7 MB (33.36%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -21,7 +21,7 @@ class Solution {
         }
         int e = nums.length-1;
         int s = index+1;
-        while(s<=e){
+        while(s<e){
             int temp = nums[s];
             nums[s] = nums[e];
             nums[e] = temp;
