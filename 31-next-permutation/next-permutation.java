a82f4ad21a8dfb7692b01aaa77df59dc8915b394
Kavish Kamal <kavishkamal437@gmail.com>
2026-09-04 01:26:48 +0530

    Time: 2 ms (6.96%) | Memory: 44.9 MB (16.09%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+        for(int i=nums.length-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            Arrays.sort(nums);
+            return;
+        }
+        for(int i=nums.length-1; i>index; i--){
+            if(nums[i]>nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        int e = nums.length-1;
+        int s = index+1;
+        while(s<=e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
+}
