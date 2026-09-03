ce586e8c3c09b17613f2ed2a1273eb6141abbdec
Kavish Kamal <kavishkamal437@gmail.com>
2026-09-03 23:56:49 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (41.22%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,30 +1,45 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        HashMap<Integer,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>();
-        int k = 0;
-        int m = 0;
-        for(int i=0; i<nums.length; i++){
-            if(i<n){
-                map1.put(k,nums[i]);
-                k++;
-            } else {
-                map2.put(m,nums[i]);
-                m++;
-            }
-        }
-        int arr[] = new int[2*n];
-        k = 0;
-        m = 0;
+        int arr[] = new int[nums.length];
+        int s = 0;
+        int e = n;
         for(int i=0; i<nums.length; i++){
             if(i%2 == 0){
-                arr[i] = map1.get(k);
-                k++;
+                arr[i] = nums[s];
+                s++;
             } else {
-                arr[i] = map2.get(m);
-                m++;
+                arr[i] = nums[e];
+                e++;
             }
         }
         return arr;
+
+        // USING HASHMAP
+        // HashMap<Integer,Integer> map1 = new HashMap<>();
+        // HashMap<Integer,Integer> map2 = new HashMap<>();
+        // int k = 0;
+        // int m = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(i<n){
+        //         map1.put(k,nums[i]);
+        //         k++;
+        //     } else {
+        //         map2.put(m,nums[i]);
+        //         m++;
+        //     }
+        // }
+        // int arr[] = new int[2*n];
+        // k = 0;
+        // m = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(i%2 == 0){
+        //         arr[i] = map1.get(k);
+        //         k++;
+        //     } else {
+        //         arr[i] = map2.get(m);
+        //         m++;
+        //     }
+        // }
+        // return arr;
     }
 }
