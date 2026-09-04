72156bd73763dcd4b36e97841d23ae80c2fbde7f
Kavish Kamal <kavishkamal437@gmail.com>
2026-09-05 00:55:25 +0530

    Time: 21 ms (95.96%) | Memory: 77.7 MB (82.28%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -2,46 +2,48 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         
         // USING SORT METHOD
-        // if(nums.length < 1){
-        //     return 0;
-        // }
-        // Arrays.sort(nums);
-        // int largest = 1;
-        // int count = 1;
-        // for(int i=0; i<nums.length-1; i++){
-        //     if(nums[i]+1 == nums[i+1]){
-        //         count++;
-        //     } else {
-        //         if(nums[i] != nums[i+1]){
-        //             count = 1;
-        //         }
-        //     }
-        //     largest = Integer.max(count, largest);
-        // }
-        // return largest;
 
-        // USING HASHSET
-        if(nums.length == 0){
+        if(nums.length < 1){
             return 0;
         }
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
-        }
+        Arrays.sort(nums);
         int largest = 1;
-
-        for(int v:set){
-            int count = 1;
-            if(!set.contains(v-1)){
-                int x = v;
-                while(set.contains(x+1)){
-                    count++;
-                    x++;
+        int count = 1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]+1 == nums[i+1]){
+                count++;
+            } else {
+                if(nums[i] != nums[i+1]){
+                    count = 1;
                 }
-                largest = Integer.max(count,largest);
             }
+            largest = Integer.max(count, largest);
         }
+        return largest;
 
-        return largest;    
+        // USING HASHSET
+
+        // if(nums.length == 0){
+        //     return 0;
+        // }
+        // Set<Integer> set = new HashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     set.add(nums[i]);
+        // }
+        // int largest = 1;
+
+        // for(int v:set){
+        //     int count = 1;
+        //     if(!set.contains(v-1)){
+        //         int x = v;
+        //         while(set.contains(x+1)){
+        //             count++;
+        //             x++;
+        //         }
+        //         largest = Integer.max(count,largest);
+        //     }
+        // }
+
+        // return largest;    
     }
 }
