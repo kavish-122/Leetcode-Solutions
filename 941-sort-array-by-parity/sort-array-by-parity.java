87166a481a68f6de70ab1c7a2a286de010e2ce00
Kavish Kamal <kavishkamal437@gmail.com>
2026-09-18 00:48:51 +0530

    Time: 1 ms (44.12%) | Memory: 46.8 MB (69.46%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,22 +1,36 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        if(nums.length == 1){
-            return nums;
-        }
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int arr[] = new int[nums.length];
-        int s = 0;
-        int sm = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%2 == 0){
-                arr[s++] = nums[i];
-            }else{
-                map.put(sm++,nums[i]);
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            while(start < nums.length && nums[start]%2 == 0){
+                start++;
+            }
+            while(end >= 0 && nums[end]%2 != 0){
+                end--;
+            }
+            if(start<=end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
             }
         }
-        for(int i=0; i<map.size(); i++){
-            arr[s++] = map.get(i);
-        }
-        return arr;
+        return nums;
+
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // int arr[] = new int[nums.length];
+        // int s = 0;
+        // int sm = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i]%2 == 0){
+        //         arr[s++] = nums[i];
+        //     }else{
+        //         map.put(sm++,nums[i]);
+        //     }
+        // }
+        // for(int i=0; i<map.size(); i++){
+        //     arr[s++] = map.get(i);
+        // }
+        // return arr;
     }
 }
